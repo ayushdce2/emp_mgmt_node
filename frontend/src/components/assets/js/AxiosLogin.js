8c1f71b22c3aa9597cbbe03becfaed23903565fb
Ayush Pratap Singh <ayushdce2@gmail.com>
2024-12-01 16:53:59 +0530

    addd frontend

--- frontend/src/components/assets/js/AxiosLogin.js
+++ frontend/src/components/assets/js/AxiosLogin.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const AxiosLogin = async (signInData)=>{
     await axios.post('https://emp-mgmt-node-4.onrender.com/api/login',signInData)
     .then((response)=>{
-        console.log('login request sent and redirected to ->',response.data.redirectUrl);
+        console.log(response.data,"<--1-->",response,"<--2-->",'login request sent and redirected to ->',response.data.redirectUrl);
         if(response.data.redirectUrl==="/admin/dashboard"){
            return window.location.href = response.data.redirectUrl;
         }else{
