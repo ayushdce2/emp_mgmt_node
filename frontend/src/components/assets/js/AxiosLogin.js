c6d4d85d3ebee74856ff60e668c310f49176a718
Ayush Pratap Singh <ayushdce2@gmail.com>
2024-12-10 14:15:27 +0530

    added

--- frontend/src/components/assets/js/AxiosLogin.js
+++ frontend/src/components/assets/js/AxiosLogin.js
@@ -1,12 +1,14 @@
-
+import {  useNavigate   } from 'react-router-dom';
 import axios from "axios";
 
 export const AxiosLogin = async (signInData)=>{
+    const navigate = useNavigate();
     await axios.post('https://emp-mgmt-node-5.onrender.com/api/login',signInData)
     .then((response)=>{
         console.log(response.data,"<--1-->",response,"<--2-->",'login request sent and redirected to ->',response.data.redirectUrl);
         if(response.data.redirectUrl==="/admin/dashboard"){
-           return window.location.href = response.data.redirectUrl;
+        //    return window.location.href = response.data.redirectUrl;
+        return navigate(response.data.redirectUrl);
         }else{
             return console.log(response.data.message,"<---axiosLogin")
         }
